a066e33c59735cf2974f77667dc04d91f69373d0
devin-olson <54080725+devin-olson@users.noreply.github.com>
2021-07-18 15:05:31 -0500

    Program that takes a user inputed postfix expression and evaluates it using stacks.

---
+++ Stacks/src/stacks/Stacks.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stacks;
+
+import java.util.*;
+/**
+ *
+ * @author Devin
+ */
+public class Stacks {
+
+    
+    
+    public static void main(String[] args) {
+        
+        PostfixStack ps = new PostfixStack();
+        
+        ps.postfixEval();
+       
+        
+        
+        
+    }
+    
+}
+
